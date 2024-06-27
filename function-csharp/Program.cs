e2a3922211b3eaae4f2b0aa434c4bb3b1f3af023
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2024-06-27 16:14:52 -0700

    wip

--- function-csharp/Program.cs
+++ function-csharp/Program.cs
@@ -1,8 +1,28 @@
 using GrpcService.Services;
+using System.Text.Json;
 
+//Args:
+// tls_certs_dir: A directory containing tls.crt, tls.key, and ca.crt.
+// function: The function (class) to use to serve requests.
+// address: The address at which to listen for requests.
+// creds: The credentials used to authenticate requests.
+// insecure: Serve insecurely, without credentials or encryption.
+// level: What log level to enable.
+//   DISABLED = 0
+//   DEBUG = 1
+//   INFO = 2
 var builder = WebApplication.CreateSlimBuilder(args);
 
-// Add services to the container.
+builder.Logging.AddJsonConsole(options =>
+{
+    options.IncludeScopes = false;
+    options.TimestampFormat = "HH:mm:ss ";
+    options.JsonWriterOptions = new JsonWriterOptions
+    {
+        Indented = true,
+    };
+});
+
 builder.Services.AddGrpc();
 
 var app = builder.Build();
--- function-csharp/Services/RunFunctionService.cs
+++ function-csharp/Services/RunFunctionService.cs
@@ -1,3 +1,4 @@
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 
 namespace GrpcService.Services;
@@ -6,6 +7,7 @@ public class RunFunctionService : FunctionRunnerService.FunctionRunnerServiceBas
 {
     private readonly ILogger<RunFunctionService> _logger;
 
+
     public RunFunctionService(ILogger<RunFunctionService> logger)
     {
         _logger = logger;
@@ -13,6 +15,42 @@ public class RunFunctionService : FunctionRunnerService.FunctionRunnerServiceBas
 
     public override Task<RunFunctionResponse> RunFunction(RunFunctionRequest request, ServerCallContext context)
     {
-        return base.RunFunction(request, context);
+        _logger.LogInformation("Running Function");
+        _logger.LogInformation(request.Meta.Tag);
+
+        var resp = request.To(Response.DefaultTTL);
+
+        return Task.FromResult(resp);
+    }
+}
+
+// https://github.com/crossplane/function-sdk-python/blob/main/crossplane/function/response.py
+public static class Response
+{
+    /// <summary>
+    /// The default TTL for which a RunFunctionResponse may be cached.
+    /// </summary>
+    public static readonly Duration DefaultTTL = Duration.FromTimeSpan(TimeSpan.FromMinutes(1));
+
+    /// <summary>
+    /// Create a response to the supplied request.
+    /// </summary>
+    /// <param name="request">The request to respond to.</param>
+    /// <param name=""></param>
+    /// <returns>A response to the supplied request.</returns>
+    public static RunFunctionResponse To(this RunFunctionRequest request, Duration ttl)
+    {
+        var resp = new RunFunctionResponse()
+        {
+            Meta = new ResponseMeta()
+            {
+                Tag = request.Meta.Tag,
+                Ttl = ttl
+            },
+            Desired = request.Desired,
+            Context = request.Context,
+        };
+
+        return resp;
     }
 }
