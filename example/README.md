389ca760ae99db95cde9dce67e15d2c3e04f6adb
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2025-08-17 01:13:19 -0700

    wip

--- example/README.md
+++ example/README.md
@@ -25,66 +25,39 @@ dotnet debug
 # Then, in another terminal, call it with these example manifests
 $ crossplane render xr.yaml composition.yaml functions.yaml
 ---
-apiVersion: azuread.company.com/v1alpha1
-kind: xApplication
+apiVersion: data.company.com/v1alpha1
+kind: ETL
 metadata:
   name: example-xr
 status:
   conditions:
   - lastTransitionTime: "2024-01-01T00:00:00Z"
-    message: 'Unready resources: app-terraform-azure-da-platform-dev-example-xr'
+    message: 'Unready resources: perm-example-xr'
     reason: Creating
     status: "False"
     type: Ready
 ---
-apiVersion: applications.azuread.upbound.io/v1beta1
-kind: Application
+apiVersion: security.databricks.crossplane.io/v1alpha1
+kind: Permissions
 metadata:
   annotations:
-    crossplane.io/composition-resource-name: app-terraform-azure-da-platform-dev-example-xr
+    crossplane.io/composition-resource-name: perm-example-xr
   generateName: example-xr-
   labels:
-    app.com/name: app-terraform-azure-da-platform-dev-example-xr
+    app.com/name: perm-example-xr
     crossplane.io/composite: example-xr
-  name: app-terraform-azure-da-platform-dev-example-xr
+  name: perm-example-xr
   ownerReferences:
-  - apiVersion: azuread.company.com/v1alpha1
+  - apiVersion: data.company.com/v1alpha1
     blockOwnerDeletion: true
     controller: true
-    kind: xApplication
+    kind: ETL
     name: example-xr
     uid: ""
 spec:
   forProvider:
-    displayName: app-terraform-azure-da-platform-dev-example-xr
-    owners:
-    - 3983c9fb-06c6-4306-b917-d9b68fe45a50
-    - owner1@example.com
-    - owner2@example.com
-    preventDuplicateNames: true
-    requiredResourceAccess:
-    - resourceAccess:
-      - id: 11111111-1111-1111-1111-111111111111
-        type: Scope
-      - id: 22222222-2222-2222-2222-222222222222
-        type: Role
-      resourceAppId: 12345678-1234-1234-1234-1234567890ab
-    singlePageApplication:
-    - redirectUris:
-      - https://example.com/callback
-      - https://example.com/redirect
-    web:
-    - homepageUrl: https://example.com
-      implicitGrant:
-      - accessTokenIssuanceEnabled: true
-        idTokenIssuanceEnabled: false
-      logoutUrl: https://example.com/logout
-      redirectUris:
-      - https://example.com/callback1
-      - https://example.com/callback2
-  managementPolicies:
-  - Observe
-  - Create
-  - Update
-  - LateInitialize
+    accessControl:
+    - groupName: my-grou-1
+      permissionLevel: CAN_MANAGE
+
 ```
--- src/Models/CompositeResource.cs
+++ src/Models/CompositeResource.cs
@@ -33,80 +33,10 @@ public partial class V1alpha1ETL : IKubernetesObject<V1ObjectMeta>, ISpec<V1alph
     public V1CompositeResourceStatus Status { get; set; }
 }
 
-public partial class V1alpha1ApplicationSpecRequiredResourceAccessResourceAccess
-{
-    /// <summary>The unique identifier for an app role or OAuth2 permission scope published by the resource application.</summary>
-    [JsonPropertyName("id")]
-    public string? Id { get; set; }
-
-    /// <summary>Specifies whether the id property references an app role or an OAuth2 permission scope. Possible values are Role or Scope.</summary>
-    [JsonPropertyName("type")]
-    public string? Type { get; set; }
-}
-
-public partial class V1alpha1ApplicationSpecRequiredResourceAccess
-{
-    /// <summary>A collection of resource_access blocks as documented below, describing OAuth2.0 permission scopes and app roles that the application requires from the specified resource.</summary>
-    [JsonPropertyName("resourceAccess")]
-    public IList<V1alpha1ApplicationSpecRequiredResourceAccessResourceAccess>? ResourceAccess { get; set; }
-
-    /// <summary>The unique identifier for the resource that the application requires access to. This should be the Application ID of the target application.</summary>
-    [JsonPropertyName("resourceAppId")]
-    public string? ResourceAppId { get; set; }
-}
-
-public partial class V1alpha1ApplicationSpecWebImplicitGrant
-{
-    /// <summary>Whether this web application can request an access token using OAuth 2.0 implicit flow. Whether this web application can request an access token using OAuth 2.0 implicit flow</summary>
-    [JsonPropertyName("accessTokenIssuanceEnabled")]
-    public bool? AccessTokenIssuanceEnabled { get; set; }
-
-    /// <summary>Whether this web application can request an ID token using OAuth 2.0 implicit flow. Whether this web application can request an ID token using OAuth 2.0 implicit flow</summary>
-    [JsonPropertyName("idTokenIssuanceEnabled")]
-    public bool? IdTokenIssuanceEnabled { get; set; }
-}
-
-public partial class V1alpha1ApplicationSpecWeb
-{
-    /// <summary>Home page or landing page of the application. Home page or landing page of the application</summary>
-    [JsonPropertyName("homepageUrl")]
-    public string? HomepageUrl { get; set; }
-
-    /// <summary>An implicit_grant block as documented above.</summary>
-    [JsonPropertyName("implicitGrant")]
-    public IList<V1alpha1ApplicationSpecWebImplicitGrant>? ImplicitGrant { get; set; }
-
-    /// <summary>The URL that will be used by Microsoft's authorization service to sign out a user using front-channel, back-channel or SAML logout protocols. The URL that will be used by Microsoft's authorization service to sign out a user using front-channel, back-channel or SAML logout protocols</summary>
-    [JsonPropertyName("logoutUrl")]
-    public string? LogoutUrl { get; set; }
-
-    /// <summary>A set of URLs where user tokens are sent for sign-in, or the redirect URIs where OAuth 2.0 authorization codes and access tokens are sent. Must be a valid http URL or a URN. The URLs where user tokens are sent for sign-in, or the redirect URIs where OAuth 2.0 authorization codes and access tokens are sent</summary>
-    [JsonPropertyName("redirectUris")]
-    public IList<string>? RedirectUris { get; set; }
-}
-
-public partial class V1alpha1ApplicationSpecSinglePageApplication
-{
-    /// <summary>A set of URLs where user tokens are sent for sign-in, or the redirect URIs where OAuth 2.0 authorization codes and access tokens are sent. Must be a valid https URL. The URLs where user tokens are sent for sign-in, or the redirect URIs where OAuth 2.0 authorization codes and access tokens are sent</summary>
-    [JsonPropertyName("redirectUris")]
-    public IList<string>? RedirectUris { get; set; }
-}
-
 public partial class V1alpha1ApplicationSpec : V1CompositeResourceSpecBase
 {
     /// <summary> A collection of user IDs that are allowed to manage this resource.</summary>
     [JsonPropertyName("owners")]
     public IList<string> Owners { get; set; }
 
-    /// <summary>A collection of required_resource_access blocks as documented below.</summary>
-    [JsonPropertyName("requiredResourceAccess")]
-    public IList<V1alpha1ApplicationSpecRequiredResourceAccess> RequiredResourceAccess { get; set; }
-
-    /// <summary>A single_page_application block as documented below, which configures single-page application (SPA) related settings for this application.</summary>
-    [JsonPropertyName("singlePageApplication")]
-    public IList<V1alpha1ApplicationSpecSinglePageApplication>? SinglePageApplication { get; set; }
-
-    /// <summary>A web block as documented below, which configures web related settings for this application.</summary>
-    [JsonPropertyName("web")]
-    public IList<V1alpha1ApplicationSpecWeb>? Web { get; set; }
 }
